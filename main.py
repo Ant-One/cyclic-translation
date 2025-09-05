99a7567e6ddd5b749eb1caa04a7869cb41858859
Antoine Moix <antoine.moix@gmail.com>
2025-09-05 14:34:59 +0200

    first try at msbt

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     #a = ct.translate("Ingénieur-stagiaire ouvrages d'art", 100)
     #print(a)
 
-    ct.translate_from_json_dir("mess_json/", "mess_trans/", max_threads=10)
+    #ct.translate_from_json_dir("mess_json/", "mess_trans/", max_threads=10)
 
-    #extractor = MSBT().to_json()
-
-    #msbt = MSBT()
+    msbt = MSBT()
 
     #msbt.from_json("/Users/antoine/cyclic-translation/mess_orig/glossary.msbt")
+    
+    msbt.from_json(input_dir="mess_trans/", orig_mess_dir="mess_orig/", output_dir="mess_trans_msbt/")
 
     #ct.translate_from_json_dir(input_dir = "mess_json", output_dir = "mess_json_trans")
 
--- msbt.py
+++ msbt.py
@@ -2,11 +2,18 @@ from pathlib import Path
 import json
 from lms.message.msbtio import read_msbt_path
 from lms.message.msbtio import write_msbt_path
+import os
+import re
 
 ORIG_DIR = "mess_orig"
 DEST_DIR = "mess_json"
 
 class MSBT:
+
+    def _remove_tags(self, input_str):
+        pattern = r'\[[^\]]*\]'
+        return re.sub(pattern, '', input_str)
+    
     def to_json(self):
         msbt_orig = Path(ORIG_DIR)
 
@@ -25,10 +32,25 @@ class MSBT:
                 with open(f"{DEST_DIR}/{msbt_file.name}.json", "w+") as file:
                     json.dump(mess_dict, file, indent=4)
 
-    def from_json(self, input):
-        orig_msbt = read_msbt_path(input)
-        a = orig_msbt.get_entry_by_index(0)
-        m = a.to_dict()
-        print(m)
+    def from_json(self, input_dir, orig_mess_dir, output_dir):
+        msbt_orig_dir = Path(orig_mess_dir)
+        for msbt_orig_file in msbt_orig_dir.iterdir():
+            if msbt_orig_file.is_file() and msbt_orig_file.suffix == '.msbt':
+                orig_msbt = read_msbt_path(str(msbt_orig_file))
+                msbt_orig_msbt_obj = read_msbt_path(str(msbt_orig_file))
 
-        a.message.text = "AAAAA"
+                with open(f"{input_dir}{os.path.basename(msbt_orig_file.name)[:-5]}_trans.msbt.json", "r") as json_trans_file:
+                    json_trans = json.load(json_trans_file)
+
+                    for mess_trans in json_trans.items():
+                        mess = msbt_orig_msbt_obj.get_entry_by_name(mess_trans[0])
+
+                        mess.message.text = self._remove_tags(mess_trans[1]['message'])
+
+            write_msbt_path(f"{output_dir}{os.path.basename(msbt_orig_file.name)}", msbt_orig_msbt_obj)       
+
+        #a = orig_msbt.get_entry_by_index(0)
+        #m = a.to_dict()
+        #print(m)
+
+        #a.message.text = "AAAAA"
